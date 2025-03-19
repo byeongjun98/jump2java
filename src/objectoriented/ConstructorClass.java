ed1996d93d408695c55e0bbcc1543ebd2dd0e89a
byeongjun98 <degea98@gmail.com>
2025-03-19 16:45:39 +0900

    Feat: 05-06 생성자까지 완료

--- src/objectoriented/ConstructorClass.java
+++ src/objectoriented/ConstructorClass.java
@@ -1,4 +1,48 @@
 package objectoriented;
 
+class Animal3 {
+    String name;
+
+    void setName(String name){
+        this.name = name;
+    }
+}
+
+class Dog3 extends Animal3{
+    void sleep(){
+        System.out.println(this.name + " zzz");
+    }
+}
+
+class HouseDog2 extends Dog3{
+    
+    HouseDog2(String name) {
+        this.setName(name);
+    }
+    HouseDog2(int type) {
+        if (type == 1) {
+            this.setName("yorkshire");
+        } else if (type == 2) {
+            this.setName("bulldog");
+        }
+    }
+    
+    void sleep(){
+        System.out.println(this.name + " zzz in house");
+    }
+
+    void sleep(int hour){
+        System.out.println(this.name + " zzz in house for " + hour + " hours");
+    }
+}
 public class ConstructorClass {
+    public static void main(String[] args) {
+        HouseDog2 houseDog2 = new HouseDog2("happy");
+        HouseDog2 yorkshire = new HouseDog2(1);
+        System.out.println(houseDog2.name);
+        System.out.println(yorkshire.name);
+//        houseDog2.setName("happy");
+//        houseDog2.sleep();
+//        houseDog2.sleep(1);
+    }
 }
